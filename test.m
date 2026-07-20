a8bf7b104e2e31e8c26c4ded3b3ab9d321cf2409
gwc <3416218577@qq.com>
2026-07-20 10:54:09 +0800

    调整布局2

--- test.m
+++ test.m
@@ -1,3 +1,6 @@
 % analyze_radar_trajectory_2("./excel_csv/still_circle.csv");
 % preprocess_raw_log("./excel_csv/still_circle.csv");
+addpath(fullfile(pwd,"function\",'pid'));
+addpath(fullfile(pwd,"function\",'track')); %添加路径
 analyze_pid_performance("./excel_csv/still_circle_radar_sync.csv");
+plot_radar_3d("./excel_csv/still_circle_radar_sync.csv");
