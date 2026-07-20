ade7eb7d68e467bf78be82c521562618ccc307ce
gwc <3416218577@qq.com>
2026-07-20 21:08:55 +0800

    我自己的滤波

--- filiter/my_flitter.m
+++ filiter/my_flitter.m
@@ -1,46 +1,49 @@
-function result = pre_log(csvPath,options)
+function result = pre_log(csvPath)
 
     
     if nargin < 1 || isempty(csvPath)
         csvPath = fullfile(fileparts(mfilename("fullpath")),'..','excel_csv','still_circle.csv');           %为什么制定的是still_circle.csv?
         csvPath = char(csvPath);
-    end
-    csvPath = char(csvPath);
-     if nargin < 2 || isempty(options)
-        options = struct();
-    end        
-    opts.dt = 0.1;
-    opts.output_dir = '';
-    opts.output_name = '';
-    flds = fieldnames(options);
-    for i = 1 : length(flds)
-        opts.(flds{i}) = options.(flds{i});
-    end
+    end     
+
 
     [rawData,colNames] = loadRawCsv(csvPath);
 
 end
 
-function [rawData,colNames] = loadRawCsv(csvPath)
+function [data,colNames] = loadRawCsv(csvPath)
 
     fid = fopen(csvPath,'r');           %指只读，而不是红色
     if fid == -1
         error('无法打开文件：%s',csvPath);
     end
 
+     % 跳过第 1 行（数字 header） CSV波形上的第一行
     fgetl(fid);
 
+    %第二行是列名
     headerline = fgetl(fid);
-    fclose(fid);
+    fclose(fid);    
 
-    colNames = strsplit(headerline,',');
+    %解析列名？？？ 这是怎么解析的？
+    colNames = strsplit(headerline,',');        %根据头文件的‘，’进行分离
     colNames = strtrim(colNames);
-    colNames(cellfun(@isempty,colNames)) = []; %去除末尾空列
+    colNames(cellfun(@isempty,colNames)) = [];  %去除末尾空列
 
     nCols = length(colNames);
     opts = detectImportOptions(csvPath,'NumVariables',nCols);
     opts.DataLines = [3,Inf];
     opts = setvartype(opts,1:nCols,'double');
     data = readmatrix(csvPath,opts);
+
+    %最后的Tick
+    lastCol = data(:,end);
+    if all(isnan(lastCol)) || (max(lastCol) < 1 && min(lastCol) > -1)
+        warning('最后一列异常')
+    end
+
+    fprintf(colNames);
+    %
+    data(all(isnan(data),2),:) = [];
 
 end
